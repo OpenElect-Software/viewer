c76edeb58c7317f24315cfb45911488f24430249
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-12 17:40:46 +0100

    initial code commit

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    output: 'standalone',
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+};
+
+export default nextConfig;
